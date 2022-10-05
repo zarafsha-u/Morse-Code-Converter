f75d5f40ce508f46a85666e630bef943fcbbd15b
zarafsha-u <zarafsha.u@gmail.com>
2022-10-05 15:21:25 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+message = input('Enter message to be converted into Morse Code! ')
+
+
+#morse code dictionary
+morse_code_dict = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    "!": "-.-.--",
+    "-": "-....-",
+    "/": "-..-.",
+    "@": ".--.-.",
+    "(": "-.--.",
+    ")": "-.--.-"
+}
+
+#Turn message in list
+message_list = [item for item in message]
+
+#Get value(morse code) of key(regular alphabet) for each item in message list into new list
+message_in_morse_list =[morse_code_dict[item] for item in message_list if item in morse_code_dict]
+
+#turn morse code list back into a string
+message_in_morse =  ' '.join(message_in_morse_list)
+print(f"Here is your message in morse code: {message_in_morse}")
+
+
